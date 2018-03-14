c900de582e6ce9ca26eec890cac76b339b9a0756
Answermouth <ansel.gamet@telecomnancy.net>
2018-03-14 19:17:33 +0100

    Ajour des JSON

--- sbc.py
+++ sbc.py
@@ -1,59 +1,95 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Mar  7 15:36:39 2018
 
-@author: poste
-"""
-
-import requests
 from SPARQLWrapper import SPARQLWrapper, JSON
-import tensorflow as tf
-#from rdflib import Graph, URIRef
-        
-sparql = SPARQLWrapper("http://dbpedia.org/sparql")
-sparql.setQuery("""
-    PREFIX cat: <http://dbpedia.org/resource/Category:>
-    PREFIX dcterms: <http://purl.org/dc/terms/>
-    PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-
-    SELECT DISTINCT ?page
-    WHERE {
-        ?subcat skos:broader* cat:Landmarks_in_the_United_States_by_state.
-        ?page dcterms:subject ?subcat
-    }
-""")
-sparql.setReturnFormat(JSON)
-results = sparql.query().convert()
-#print(results)
-
-for result in results['results']['bindings']:
-    print (result['page']['value'])
-
-    sparql.setQuery("""
-        PREFIX dcterms: <http://purl.org/dc/terms/>
-    
-        SELECT DISTINCT ?subject
-        WHERE {
-            <"""+result['page']['value']+"""> dcterms:subject* ?subject
-        }
-    """)
-    sparql.setReturnFormat(JSON)
-    subjects = sparql.query().convert()
-    #print(subjects)
-    subjectList=[]
-    for subject in subjects['results']['bindings']:
-        subjectList.append(subject['subject']['value'])
-        
-    subjectList.pop(0)
-    result['page']['subjects']=subjectList
-
-
-    #break
-    
-
-print('finished')
-
+from pathlib import Path
 import json
-with open('result.json', 'w') as fp:
-    json.dump(results, fp)
+from time import sleep
+
+sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+sparql.setTimeout(20)
+results = {}
+my_file = Path("./pages.json")
+
+if my_file.is_file():    
+    print("Opening file")
+    results = json.load(open('pages.json'))
+        
+else:
+    print("Requesting data")
+    categories = [
+            "Landmarks_in_the_United_States_by_state",
+            "Tennis",
+            "Internet_service_providers",
+            "Computer_viruses",
+            "Mercedes-Benz",
+            "Credit_cards",
+            "Fictional_horses",
+            "Food_science",
+            "French_physicists",
+            "Discordianism"]
+    
+    for cat in categories:
+        sparql.setQuery("""
+            PREFIX cat: <http://dbpedia.org/resource/Category:>
+            PREFIX dcterms: <http://purl.org/dc/terms/>
+            PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+        
+            SELECT DISTINCT ?page
+            WHERE {
+                ?subcat skos:broader* cat:""" + cat + """.
+                ?page dcterms:subject ?subcat
+            } LIMIT 100
+        """)
+        
+        sparql.setReturnFormat(JSON)
+        resultsForOneCat = sparql.query().convert()
+        
+        if results == {} :
+            results = resultsForOneCat
+        else :
+            results["results"]["bindings"]+=resultsForOneCat["results"]["bindings"]
+            
+        with open('pages.json', 'w') as fp:
+            json.dump(results, fp)
+
+
+my_file = Path("./pagesWithSubjects.json")
+
+print(results)
+
+if my_file.is_file():    
+    print("Opening file")
+    data = json.load(open('pagesWithSubjects.json'))
+else:
+    print("Requesting additionnal data")
+    for result in results['results']['bindings']:
+        print (result['page']['value'])
+        #sleep(0.1)
+    
+        sparql.setQuery("""
+            PREFIX dcterms: <http://purl.org/dc/terms/>
+        
+            SELECT DISTINCT ?subject
+            WHERE {
+                <"""+result['page']['value']+"""> dcterms:subject* ?subject
+            }
+        """)
+        sparql.setReturnFormat(JSON)
+        subjects = sparql.query().convert()
+        subjectList=[]
+        for subject in subjects['results']['bindings']:
+            subjectList.append(subject['subject']['value'])
+            
+        subjectList.pop(0)
+        result['page']['subjects']=subjectList
+        #break
+        with open('pagesWithSubjects.json', 'w') as fp:
+            json.dump(results, fp)
+            
+    print('finished')
+
+
+
+    
+    
